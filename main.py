6aaf16187c1ce6b11796d67568bc9ab61ebcd9c5
Brian Rudacille <whitewolfecat@yahoo.com>
2017-01-29 22:53:08 -0600

    added field names

--- main.py
+++ main.py
@@ -14,47 +14,135 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-<form>
-    <input name ="q">
-    <input type ="submit">
-</form>
 
 import webapp2
 
-form="""
-<form action ="/Signup">
-    <input name ="q">
-    <input type ="submit">
-</form>
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
 """
 
-#class MainHandler(webapp2.RequestHandler):
-#    def get(self):
-#        self.response.write('Heeeeello world!')
+page_footer = """
+</body>
+</html>
+"""
 
-#app = webapp2.WSGIApplication([
-#    ('/', MainHandler)
-#], debug=True)
+table_head = """
+    <table>
+        <tbody>
+"""
+
+table_foot = """
+        </tbody>
+    </table>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write(form)
 
-class Signup(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write(form)
+        edit_header = "<h1>Signup</h1>"
 
-    def post(self):
-        have_error = False
-        username = self.request.get('username')
-        password = self.request.get('password')
-        verify = self.request.get('verify')
-        email = self.request.get('email')
+        add_name = """
+        <form action="/name" method="post">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label>
+                                Username
+                            </label>
+                        </td>
+                        <td>
+                            <label>
+                                <input type="text" name="username" value required/>
+                            </label>
+                        </td>
+                    </tr>
+        </form>
+        """
+
+        add_password = """
+        <form action="/password" method="post">
+                    <tr>
+                        <td>
+                            <label>
+                                Password
+                            </label>
+                        </td>
+                        <td>
+                            <label>
+                                <input type="text" name="password"/>
+                            </label>
+                        </td>
+                    </tr>
+        </form>
+        """
+
+        verify_password = """
+        <form action="/verify" method="post">
+                    <tr>
+                        <td>
+                            <label>
+                                Verify Password
+                            </label>
+                        </td>
+                        <td>
+                            <label>
+                                <input type="text" name="verify"/>
+                            </label>
+                        </td>
+                    </tr>
+        </form>
+        """
+
+        add_email = """
+        <form action="/email" method="post">
+                    <tr>
+                        <td>
+                            <label>
+                                Email (optional)
+                            </label>
+                        </td>
+                        <td>
+                            <label>
+                                <input type="text" name="password"/>
+                            </label>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type ="submit"/>
+        </form>
+        """
+
+        main_content = edit_header + add_name + add_password + verify_password + add_email
+        content = page_header + main_content + page_footer
+        self.response.write(content)
+
+#class Signup(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.out.write(form)
+#
+#    def post(self):
+#        have_error = False
+#        username = self.request.get('username')
+#        password = self.request.get('password')
+#        verify = self.request.get('verify')
+#        email = self.request.get('email')
 
 #class Welcome(BaseHandler):
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/unit2/signup' Signup)
-#    ('/unit/welcome', Welcome)
+    ('/', MainPage)
+#    ('/name' username)
+#    ('/password', password)
 ], debug=True)
